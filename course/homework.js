9725754e0bc02427317209ebf8daaf729ad13691
Eitan <8an.akrish@gmail.com>
2021-11-16 11:40:12 +0200

    no major changes

--- course/homework.js
+++ course/homework.js
@@ -139,3 +139,211 @@
 //         counter++
 //     } else {}
 // }
+
+
+// class 05
+// homework     
+
+
+// function splitAniCode(zooArray, method) {
+//     let animals = [],
+//         codes = [];
+//     zooArray = zooArray.toLowerCase().split(`, `)
+//     for (i = 0; i < zooArray.length; i++) {
+//         zooAnimals = (zooArray[i].slice(0, zooArray[i].search(/\d/)));
+//         animalCodes = zooArray[i].replace(zooAnimals, '');
+//         codes.push(animalCodes)
+//         animals.push(zooAnimals)
+//     }
+//     if (method == `code`) {
+//         return codes
+//     } else {
+//         return animals
+//     }
+// }
+
+// function search(array, str) {
+//     for (i = 0; i < array.length; i++) {
+//         if (codeArr[i] == str) {
+//             return i
+//         }
+//     }
+// }
+
+// function searchCode(str) {
+//     while (isNaN(search(codeArr, str))) {
+//         alert(`code not found`)
+//         str = prompt(`enter animal code`)
+//     }
+//     return alert(`${animalArr[search(codeArr, str)].charAt(0).toUpperCase() + animalArr[search(codeArr, str)].substring(1)} || ${codeArr[search(codeArr, str)]}`)
+
+// }
+
+// function searchAnimal(str) {
+//     let searchByName = `search input: ${str}\nthe following animals were found:`
+//     for (i = 0; i < animalArr.length; i++) {
+//         if (animalArr[i].search(str) != -1) {
+//             searchByName += `\nAnimal name: ${animalArr[i].charAt(0).toUpperCase() + animalArr[i].substring(1)}\nAnimal code: ${codeArr[i]}\n`
+//         }
+//     }
+//     return searchByName
+// }
+
+// function addAnimal(name, code) {
+//     while (!isNaN(search(codeArr, newCode))) {
+//         alert(`code already taken`)
+//         newCode = Number(prompt(`enter new code`))
+//     }
+//     animalArr.push(newAnimal.toLowerCase())
+//     codeArr.push(String(newCode))
+// }
+
+// function deleteAnimal(str) {
+//     while (isNaN(search(codeArr, str))) {
+//         str = prompt(`this code is not being used enter existing animal code`)
+//     }
+//     let i = search(codeArr, str)
+//     animalArr.splice(i, 1)
+//     codeArr.splice(i, 1)
+
+// }
+// const txt1 = "Dog12, CAT3, LiOn7, DolphiN11, fish6 ";
+// const txt2 = "PIG17, bear29, BiRd8, SNAKE39, donkey14";
+// let animalArr = splitAniCode(txt1).concat(splitAniCode(txt2))
+// codeArr = splitAniCode(txt1, `code`).concat(splitAniCode(txt2, `code`))
+// let options = Number(prompt(`choose a option:\n1. search animal by code\n 2. search animal by name\n 3. add new animal\n 4. erase old animal`))
+
+
+// while (options != -1) {
+
+//     switch (options) {
+//         case 1:
+//             str = prompt(`enter animal code`)
+//             searchCode(str)
+//             break;
+//         case 2:
+//             str = prompt(`enter animal name`).toLowerCase()
+//             alert(searchAnimal(str))
+//             break;
+//         case 3:
+//             newAnimal = prompt(`enter animal name`)
+//             newCode = Number(prompt(`enter new code`))
+//             addAnimal(newAnimal, newCode)
+//             break;
+//         case 4:
+//             str = prompt(`enter animal code`)
+//             deleteAnimal(str)
+//             break;
+//         default:
+//             options = Number(prompt(`choose a option:\n1. search animal by code\n 2. search animal by name\n 3. add new animal\n 4. erase old animal`))
+//     }
+//     console.log(animalArr);
+//     console.log(codeArr);
+//     options = Number(prompt(`choose a option:\n1. search animal by code\n 2. search animal by name\n 3. add new animal\n 4. erase old animal`))
+// }
+
+
+
+
+
+// short
+
+// const txt1 = "Dog12, CAT3, LiOn7, DolphiN11, fish6 ";
+// const txt2 = "PIG17, bear29, BiRd8, SNAKE39, donkey14";
+// let animalArr = [],
+//     codeArr = [],
+//     options;
+// splitAniCode(txt1)
+// splitAniCode(txt2)
+// console.log(animalArr);
+// console.log(codeArr);
+
+// function splitAniCode(zooArray) {
+//     zooArray = zooArray.toLowerCase().split(`, `)
+//     for (i = 0; i < zooArray.length; i++) {
+//         zooAnimals = (zooArray[i].slice(0, zooArray[i].search(/\d/)));
+//         animalCodes = zooArray[i].replace(zooAnimals, '');
+//         animalArr.push(zooAnimals)
+//         codeArr.push(animalCodes)
+//     }
+// }
+
+// function search(array, str) {
+//     for (i = 0; i < array.length; i++) {
+//         if (codeArr[i] == str) {
+//             return i
+//         }
+//     }
+// }
+
+// function searchCode(str) {
+//     while (isNaN(search(codeArr, str))) {
+//         alert(`code not found`)
+//         str = prompt(`enter animal code`)
+//     }
+//     return alert(`${animalArr[search(codeArr, str)].charAt(0).toUpperCase() + animalArr[search(codeArr, str)].substring(1)} || ${codeArr[search(codeArr, str)]}`)
+
+// }
+
+// function searchAnimal(str) {
+//     let searchByName = `search input: ${str}\nthe following animals were found:`
+//     for (i = 0; i < animalArr.length; i++) {
+//         if (animalArr[i].search(str) != -1) {
+//             searchByName += `\nAnimal name: ${animalArr[i].charAt(0).toUpperCase() + animalArr[i].substring(1)}\nAnimal code: ${codeArr[i]}\n`
+//         }
+//     }
+//     return searchByName
+// }
+
+// function addAnimal(name, code) {
+//     while (!isNaN(search(codeArr, newCode))) {
+//         alert(`code already taken`)
+//         newCode = Number(prompt(`enter new code`))
+//     }
+//     animalArr.push(newAnimal.toLowerCase())
+//     codeArr.push(String(newCode))
+// }
+
+// function deleteAnimal(str) {
+//     while (isNaN(search(codeArr, str))) {
+//         str = prompt(`this code is not being used enter existing animal code`)
+//     }
+//     let i = search(codeArr, str)
+//     animalArr.splice(i, 1)
+//     codeArr.splice(i, 1)
+
+// }
+
+
+// do {
+//     options = Number(prompt(`choose a option:\n1. search animal by code\n 2. search animal by name\n 3. add new animal\n 4. erase old animal`))
+
+//     switch (options) {
+//         case 1:
+//             str = prompt(`enter animal code`)
+//             searchCode(str)
+//             break;
+//         case 2:
+//             str = prompt(`enter animal name`).toLowerCase()
+//             alert(searchAnimal(str))
+//             break;
+//         case 3:
+//             newAnimal = prompt(`enter animal name`)
+//             newCode = Number(prompt(`enter new code`))
+//             addAnimal(newAnimal, newCode)
+//             break;
+//         case 4:
+//             str = prompt(`enter animal code`)
+//             deleteAnimal(str)
+//             break;
+
+//         case 5:
+//             options = 5
+//             break;
+
+//         default:
+//             options = Number(prompt(`choose a option:\n1. search animal by code\n 2. search animal by name\n 3. add new animal\n 4. erase old animal`))
+//     }
+//     console.log(animalArr);
+//     console.log(codeArr);
+// } while (options != 5);
